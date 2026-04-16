466843fac95e3702c83876392588fab5f13529a7
brennotortoza <brenno.tortoza@gmail.com>
2026-04-16 13:00:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 header("Content-Type: application/json; charset=UTF-8");
 
 echo json_encode([
-    "projeto" => "API TCC",
-    "status" => "online",
-    "autor" => "Seu Nome"
+    "projeto" => "Float",
+    "status" => "online"
 ]);
