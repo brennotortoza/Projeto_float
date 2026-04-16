d9e7401aae3e6ff02fe5b3e843a25fe4da2d4d3c
brennotortoza <brenno.tortoza@gmail.com>
2026-04-16 13:59:39 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,10 +1,12 @@
 <?php
-$host = "mysql.railway.internal";
-$user = "root";
-$pass = "uYbOgZGxOSIZVefbqwOsXZIKDasRtjrd";
-$db   = "railway";
-$port = "3306";
+// Pega os valores reais que estão guardados no painel do Railway
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE'); // Verifique se no Railway está sem o underline (_)
+$port = getenv('MYSQLPORT');
 
+// FAZ A CONEXÃO USANDO AS VARIÁVEIS (SEM ASPAS NOS NOMES)
 $ocon = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$ocon) {
